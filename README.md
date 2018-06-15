b846ed7547f2478c56c564377b68ee14e38991f1
Arthur Thomas IV <acthomasiv@gmail.com>
2018-06-15 14:01:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# gradBot
---
+++ main.js
@@ -0,0 +1,17 @@
+// calls to html rivescript
+/* Ideas for accomplishing this. For the chat bot like look and exeperience, i could append each reply 
+into a list then text align each element on the right of left side of the screen or using two columns, 
+i could append one item into one column at a differnet row and increment per reply. Or i could add div elements one after the other.
+
+*/
+function main() {
+    submit.onclick = chat;
+
+}
+
+
+function chat() {
+    let userInput = chatInput.value;
+    //reply.innerHTML = userInput;
+
+}
