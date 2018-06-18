a7acb5f34259d9c898a86b2982eccb351043f893
Arthur Thomas IV <acthomasiv@gmail.com>
2018-06-18 13:50:19 -0400

    Updated js and added a full rive reply
    I added some of the code so that the bot will load and reply in console.

--- main.js
+++ main.js
@@ -2,19 +2,31 @@
 /* Ideas for accomplishing this. For the chat bot like look and exeperience, i could append each reply 
 into a list then text align each element on the right of left side of the screen or using two columns, 
 i could append one item into one column at a differnet row and increment per reply. Or i could add div elements one after the other or into an array than print div by div.
+//console.log("Improvements or suggestions about this chatbot are appreciated, please email thomas iv at umd edu");
+
 
 */
+
 function main() {
     submit.onclick = chat;
+    bot = new RiveScript();
+    bot.loadFile("grad.rive", botReady, botError);
 
+
+    function botReady() {
+        console.log("Chatbot Status: OK");
+        bot.sortReplies();
+    }
+
+    // It's good to catch errors too!
+    function botError(error) {
+        console.log("Chatbot Status: Failed" + error);
+    }
 }
-
-
-
 
 function chat() {
-    let userInput = chatInput.value;
+    let userInput = chatInput.value.trim();
     //reply.innerHTML = userInput;
-
+    let reply = bot.reply("local-user", userInput);
+    console.log(reply);
 }
-
