72306de2643a86a0c51ee1f88c8555a8f7fe1536
Arthur Thomas <acthomasiv@gmail.com>
2018-06-17 22:52:43 -0400

    Added notes
    brain storming notes added.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // calls to html rivescript
 /* Ideas for accomplishing this. For the chat bot like look and exeperience, i could append each reply 
 into a list then text align each element on the right of left side of the screen or using two columns, 
-i could append one item into one column at a differnet row and increment per reply. Or i could add div elements one after the other.
+i could append one item into one column at a differnet row and increment per reply. Or i could add div elements one after the other or into an array than print div by div.
 
 */
 function main() {
@@ -10,8 +10,11 @@ function main() {
 }
 
 
+
+
 function chat() {
     let userInput = chatInput.value;
     //reply.innerHTML = userInput;
 
 }
+
