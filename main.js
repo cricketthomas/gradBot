e21a5469abb9699792a949aa9ac14f32f60e6132
Arthur Thomas <acthomasiv@gmail.com>
2018-06-24 22:05:07 -0400

    Added code to make the JSON better. Added CSS, experimenting with table output. Might try to do appending divs for each new element?
    So, i wasn't formatting the JSON properly, I was but not in the best manner. I was using literal rather than object. So i researched and found a method that applied keys then dived up the messages better. Muc better way to create json. the original way wasnt bad but it made it harder to output. Upnext, style and staggering messages. Also added CSS file.

--- main.js
+++ main.js
@@ -28,15 +28,12 @@ function botError(error) {
 
 let botMessagesArray = [];
 let userMessagesArray = [];
-let messagesJson = {};
+var messagesJson = {};
 
 function chat() {
     // add a do while loop so they cant hit it while blank or only make the button visible then.
     let userInput = chatInput.value.trim();
     let reply = bot.reply("local-user", userInput);
-    let userOutput = document.getElementById("userOutput");
-    let botReply = document.getElementById("botReply");
-    let test = document.getElementById('test');
 
     //console.log("You: " + userInput);
     let promiseReply = Promise.resolve(reply);
@@ -55,59 +52,84 @@ function chat() {
 
 
 let count = 0;
-//this function converts messages to JSON or literal?
+//this seperates each one into a json format better than i previously had. 
 function msgJSON() {
-    for (let i = 0; i < userMessagesArray.length && i < botMessagesArray.length; i++) {
-        messagesJson["user" + (i + 1)] = userMessagesArray[i];
-        messagesJson["bot" + (i + 1)] = botMessagesArray[i];
-        count++;
+    var message = ['user', 'bot'];
+    var messages = [userMessagesArray, botMessagesArray];
+
+    function arrMerge(key, value) {
+        var object = {};
+        for (var i = 0; i < key.length; i++) {
+            object[key[i]] = value[i];
+            count++;
+        }
+        return object;
     }
-    //messagesJson = JSON.stringify(messagesJson);
-    console.log(JSON.stringify(messagesJson));
-    test.innerHTML = JSON.stringify(messagesJson); //maybe print it as json?
+    let messagesCombination = arrMerge(message, messages);
+    let json = JSON.stringify(messagesCombination);
+    parsedMsg = JSON.parse(json)
+    console.log(parsedMsg);
+    console.log(parsedMsg.user)
+    console.log(parsedMsg.bot)
+    newMessage();
+    //userOutput.innerHTML = parsedMsg.user;
+    //botReply.innerHTML = parsedMsg.bot;
 }
 
+//let userOutput = document.getElementById("userOutput");
+//let botReply = document.getElementById("botReply");
 
+function newMessage() {
+    var table = document.getElementById("conversation");
+    var botRow = document.getElementById("botRow");
+    var row = table.insertRow(-1);
+    var q = row.insertCell(0);
+    var a = row.insertCell(1);
+    a.innerHTML = parsedMsg.bot.slice(-1)[0];   
+    q.innerHTML = parsedMsg.user.slice(-1)[0];
+}
 
+//trying to figure out style for each row to be staggered.
+
+/* 
+
+//playground for new methods. 
 //Write a function that prints stringified json. 
 let cleanJson = JSON.stringify(messagesJson);
-for (let x = 0; x < userMessagesArray.length, x++;) {
+for (let x = 0; x < userMessagesArray.length, x++;) {}
 
 
+// does this print the json more readable?
+for (var key in messagesJson) {
+    if (messagesJson.hasOwnProperty(key)) {
+        var val = messagesJson[key];
+        console.log(val);
+    }
 }
 
-
-
-for (var key in cleanJson) {
-  if (cleanJson.hasOwnProperty(key)) {
-    var val = cleanJson[key];
-    console.log(val);
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 function divJson() {
     Object.keys(messagesJson).forEach(function (key) {
         for (x in messagesJson) {
-
-
             console.log("user: " + messagesJson[key]);
         }
     });
     Object.keys(replyJson).forEach(function (key) {
         console.log("bot: " + replyJson[key]);
     })
+
+
+
+    //this function converts messages to JSON or literal?
+function msgJSON_OLD() {
+    for (let i = 0; i < userMessagesArray.length && i < botMessagesArray.length; i++) {
+        messagesJson["user" + (i + 1)] = userMessagesArray[i];
+        messagesJson["bot" + (i + 1)] = botMessagesArray[i];
+        count++;
+    }
+    //messagesJson = JSON.stringify(messagesJson);
+    var msg = "Message " + JSON.stringify(messagesJson, undefined, 3);
+    test.innerHTML = msg; //maybe print it as json?
+    console.log(msg);
+}
+
 } */
